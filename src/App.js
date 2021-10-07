eb7a4d36533d79292564fe804730b1c4d4a2c250
John Laurence Ramacola <johnlaurenceramacola@Johns-MBP.lan>
2021-10-08 00:10:48 +0100

    added header-component to home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      
+      <Header />
       <Switch> 
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
